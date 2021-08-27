f786f4e63a66ec9fb879f1a3538209657f655b0e
Brandon Walderman <brwalder@microsoft.com>
2021-08-27 16:32:32 -0700

    Move server/mapper build outputs to separate folders. Add config for content scripts.

---
+++ src/bidiMapper/scripts/rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
+
+export default [
+  {
+    input: 'src/bidiMapper/scripts/eval.ts',
+    output: {
+      file: 'src/.build/scripts/eval.txt',
+      sourcemap: true,
+      format: 'iife',
+    },
+    plugins: [
+      nodeResolve({ browser: true }),
+      typescript({
+        tsconfig: 'src/bidiMapper/scripts/tsconfig.scripts.json',
+      }),
+    ],
+  },
+];
