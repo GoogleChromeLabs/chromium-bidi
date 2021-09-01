04d9fc73a02e82ecadb27ed520dc7fee0108d1ff
Brandon Walderman <brwalder@microsoft.com>
2021-09-01 14:19:45 -0700

    Implement prototype evaluate script command.

--- src/bidiMapper/domains/context/context.ts
+++ src/bidiMapper/domains/context/context.ts
@@ -16,12 +16,28 @@
  */
 
 import { Protocol } from 'devtools-protocol';
+import { CdpClient } from '../../../cdp';
+
+import EVALUATOR_SCRIPT from '../../scripts/eval.txt';
 
 export class Context {
   _targetInfo?: Protocol.Target.TargetInfo;
   _sessionId?: string;
 
-  constructor(private _contextId: string) {}
+  private constructor(
+    private _contextId: string,
+    private _cdpClient: CdpClient
+  ) {}
+
+  public static create(contextId: string, cdpClient: CdpClient) {
+    const context = new Context(contextId, cdpClient);
+    context._initialize();
+    return context;
+  }
+
+  private _initialize() {
+    // TODO: Subscribe to Runtime and Page events to track executionContexts and frames.
+  }
 
   _setSessionId(sessionId: string): void {
     this._sessionId = sessionId;
@@ -45,5 +61,46 @@ export class Context {
       parent: this._targetInfo!.openerId ? this._targetInfo!.openerId : null,
       url: this._targetInfo!.url,
     };
+  }
+
+  public async evaluateScript(script: string, args: any[]) {
+    // Construct a javascript string that will call the evaluator function
+    // with the user script (embedded as a string), and the user arguments
+    // (embedded as BiDi RemoteValue objects). The result is a JSON object
+    // which will be returned by value and may be either:
+    //   { result: <value returned from user script> }
+    // or,
+    //   { error: { message: '<error message from user script>', stacktrace? } }
+
+    const expression = `(${EVALUATOR_SCRIPT}).apply(null, [${JSON.stringify(
+      script
+    )}, ${JSON.stringify(args)}])`;
+    const { result, exceptionDetails } = await this._cdpClient.Runtime.evaluate(
+      {
+        expression,
+        returnByValue: true,
+      }
+    );
+
+    // TODO: Should we let exceptions thrown from the user script bubble up to here,
+    // or catch them and send an { error: .. } value?
+    if (exceptionDetails) {
+      throw new Error(exceptionDetails.text);
+    }
+
+    // Runtime.evaluate did not return a result for some reason. Could be an
+    // internal error, or a failure to parse the user's script.
+    const value = result.value;
+    if (!value) {
+      throw new Error('unable to parse result from evaluateScript');
+    }
+
+    // Evaluator script ran but returned an error.
+    if (value.error) {
+      throw new Error(value.error);
+    }
+
+    // Evaluator returned a valid result object. Return to caller.
+    return value.result;
   }
 }
