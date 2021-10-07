536afd4bc0d08e403e1c39ccd52c26def2dea9cc
Maksim Sadym <sadym@chromium.org>
2021-10-07 09:15:27 +0000

    Command typification.

--- src/bidiMapper/bidiProtocolTypes.ts
+++ src/bidiMapper/bidiProtocolTypes.ts
@@ -2,6 +2,7 @@ export namespace CommonDataTypes {
   // TODO sadym: declare `RemoteValue` properly according to
   // https://w3c.github.io/webdriver-bidi/#type-common-RemoteValue.
   export type RemoteValue = any;
+  export type EmptyParams = {};
 
   export type ExceptionDetails = {
     columnNumber: number;
@@ -10,9 +11,11 @@ export namespace CommonDataTypes {
     stackTrace: StackTrace;
     text: string;
   };
+
   export type StackTrace = {
     callFrames: StackFrame[];
   };
+
   export type StackFrame = {
     url: string;
     functionName: string;
@@ -20,14 +23,22 @@ export namespace CommonDataTypes {
     columnNumber: number;
   };
 }
+
 export namespace Script {
-  export type RealTarget = {
+  export type RealmTarget = {
     // TODO sadym: implement.
   };
+
   export type ContextTarget = {
     context: BrowsingContext.BrowsingContext;
   };
-  export type Target = ContextTarget | RealTarget;
+
+  export type Target = ContextTarget | RealmTarget;
+
+  export type ScriptEvaluateCommand = {
+    method: 'script.evaluate';
+    params: ScriptEvaluateParameters;
+  };
 
   export type ScriptExceptionResult = {
     exceptionDetails: CommonDataTypes.ExceptionDetails;
@@ -48,6 +59,11 @@ export namespace Script {
   };
 
   export namespace PROTO {
+    export type ScriptInvokeCommand = {
+      method: 'PROTO.script.invoke';
+      params: ScriptInvokeParameters;
+    };
+
     export type ScriptInvokeParameters = {
       functionDeclaration: string;
       args: InvokeArgument[];
@@ -74,9 +90,59 @@ export namespace Script {
       value: any;
     };
   }
+}
 
-  // https://w3c.github.io/webdriver-bidi/#module-browsingContext
-  export namespace BrowsingContext {
-    export type BrowsingContext = string;
-  }
+// https://w3c.github.io/webdriver-bidi/#module-browsingContext
+export namespace BrowsingContext {
+  export type BrowsingContext = string;
+
+  export type BrowsingContextGetTreeCommand = {
+    method: 'browsingContext.getTree';
+    params: BrowsingContextGetTreeParameters;
+  };
+
+  export type BrowsingContextGetTreeParameters = {
+    maxDepth?: number;
+    parent?: BrowsingContext;
+  };
+
+  export type BrowsingContextGetTreeResult = {
+    contexts: BrowsingContextInfoList;
+  };
+
+  export type BrowsingContextInfoList = BrowsingContextInfo[];
+
+  export type BrowsingContextInfo = {
+    context: BrowsingContext;
+    parent?: BrowsingContext;
+    url: string;
+    children: BrowsingContextInfoList;
+  };
+
+  // `browsingContext.create`
+  export type BrowsingContextCreateCommand = {
+    method: 'browsingContext.create';
+    params: BrowsingContextCreateParameters;
+  };
+
+  export type BrowsingContextCreateType = 'tab' | 'window';
+
+  export type BrowsingContextCreateParameters = {
+    type: BrowsingContextCreateType;
+  };
+  export type BrowsingContextCreateResult = {
+    context: BrowsingContext;
+  };
+}
+
+export namespace Session {
+  export type SessionStatusCommand = {
+    method: 'session.status';
+    params: CommonDataTypes.EmptyParams;
+  };
+
+  export type SessionStatusResult = {
+    ready: boolean;
+    message: string;
+  };
 }
