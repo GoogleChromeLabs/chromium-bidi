35d060c3805f5f2ff84c2d2bdd83ce316a235e41
Maksim Sadym <sadym@chromium.org>
2021-11-22 09:18:26 +0000

    Add comments

--- tests/example.py
+++ tests/example.py
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# This script implemets Puppeteer's `examples/cross-browser.js` scenario using WebDriver BiDi.
+# https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js
+
 import asyncio
 import json
 import os
@@ -32,28 +35,79 @@ async def read_JSON_message(websocket):
     return json.loads(await websocket.recv())
 
 async def main():
+    # const browser = await puppeteer.launch();
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L29
     websocket = await get_websocket()
 
+    # Receive initial `browsingContext.contextCreated` event.
     resp = await read_JSON_message(websocket)
     assert resp['method'] == 'browsingContext.contextCreated'
 
-    contextID = resp["params"]["context"]
+    # Not implemented:
+    # await browser.version());
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L32
 
-    # Navigate to 'https://news.ycombinator.com/'.
+    # Puppeteer:
+    # const page = await browser.newPage();
+
+    # Part 1. Execute BiDi command.
+    # ... browser.newPage();
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L31
     command = {
         "id": 1000,
+        "method": "PROTO.browsingContext.create",
+        "params": {}}
+    await send_JSON_command(websocket, command)
+
+    # Receive `browsingContext.contextCreated` event.
+    resp = await read_JSON_message(websocket)
+    assert resp['method'] == 'browsingContext.contextCreated'
+
+    # Part 2. Wait for result.
+    # ... await ...
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L31
+    resp = await read_JSON_message(websocket)
+    assert resp["id"] == 1000
+
+    # Part 3. Get the command result.
+    # const page = ...
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L31
+    contextID = resp['result']['context']
+
+    # Puppeteer:
+    # const page = await browser.newPage();
+
+    # Part 1. Execute BiDi command.
+    # ... page.goto('https://news.ycombinator.com/');
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L34
+    command = {
+        "id": 1001,
         "method": "browsingContext.navigate",
         "params": {
             "url": "https://news.ycombinator.com/",
             "context": contextID}}
     await send_JSON_command(websocket, command)
 
+    # Part 2. Wait for result.
+    # await ...;
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L34
     resp = await read_JSON_message(websocket)
-    assert resp["id"] == 1000
+    assert resp["id"] == 1001
 
-    # Send command.
+    # const resultsSelector = '.storylink';
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L37
+    resultsSelector = {
+                "type":"string",
+                "value":".titlelink"}
+
+    # Puppeteer:
+    # const links = await page.evaluate((resultsSelector) => {...}, resultsSelector);
+
+    # Part 1. Execute BiDi command.
+    # ... page.evaluate((resultsSelector) => {...}, resultsSelector);
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L38
     await send_JSON_command(websocket, {
-        "id": 1001,
+        "id": 1002,
         "method": "PROTO.script.callFunction",
         "params": {
             "functionDeclaration": """(resultsSelector) => {
@@ -63,15 +117,25 @@ async def main():
                     return `${title} - ${anchor.href}`;
                 });
             }""",
-            "args": [{
-                "type":"string",
-                "value":".titlelink"}],
+            "args": [resultsSelector],
             "target": {"context": contextID}}})
 
+    # Part 2. Wait for result.
+    # ... await ...
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L38
     resp = await read_JSON_message(websocket)
-    assert resp["id"] == 1001
+    assert resp["id"] == 1002
 
-    for item in resp["result"]["result"]["value"]:
+    # Part 3. Get the command result.
+    # const links = ...
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L38
+    links = resp["result"]["result"]
+    assert links["type"] == "array"
+
+    # Puppeteer:
+    # console.log(links.join('\n'));
+    # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L45
+    for item in links["value"]:
         print(item["value"])
 
 loop = asyncio.new_event_loop()
