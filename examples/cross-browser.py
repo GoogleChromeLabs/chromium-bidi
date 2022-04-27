24f7b0421a8a954af3d1a5f0787b357a7f02aabe
Mathias Bynens <mathias@qiwi.be>
2022-04-27 18:10:57 +0200

    Update examples/cross-browser.py
    Co-authored-by: Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>

--- examples/cross-browser.py
+++ examples/cross-browser.py
@@ -85,8 +85,8 @@ async def main():
     # Puppeteer:
     # await page.goto('https://news.ycombinator.com/');
     # https://github.com/puppeteer/puppeteer/blob/4c3caaa3f99f0c31333a749ec50f56180507a374/examples/cross-browser.js#L34
-    appUrl = f'file://{Path(__file__).parent.resolve()}/app.html'
-    print(appUrl)
+    requestedPagePath = f'file://{Path(__file__).parent.resolve()}/app.html'
+    print(requestedPagePath)
     await run_and_wait_command({
         "id": 1001,
         "method": "browsingContext.navigate",
