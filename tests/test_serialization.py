58456926e9331b4e95acd12dfcf2ddb7fac7502b
Maksim Sadym <sadym@google.com>
2022-05-06 11:59:28 +0200

    e2e serialization tests

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -23,7 +23,8 @@ async def assertSerialization(websocket, context_id, js_str_object,
         "method": "script.evaluate",
         "params": {
             "expression": f"({js_str_object})",
-            "target": {"context": context_id}}})
+            "target": {"context": context_id},
+            "awaitPromise": False, }})
 
     # Compare ignoring `objectId`.
     recursiveCompare(expected_serialized_object, result, ["objectId"])
@@ -67,6 +68,9 @@ async def test_deserialization_serialization_string(websocket, context_id):
     await assertDeserializationAndSerialization(websocket, context_id, {
         "type": "string",
         "value": "someStr"})
+    await assertDeserializationAndSerialization(websocket, context_id, {
+        "type": "string",
+        "value": ""})
 
 
 @pytest.mark.asyncio
@@ -111,6 +115,52 @@ async def test_serialization_function(websocket, context_id):
     await assertSerialization(websocket, context_id,
                               "function(){}", {
                                   "type": "function",
+                                  "objectId": "__any_value__"
+                              })
+
+
+@pytest.mark.asyncio
+async def test_serialization_promise(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "Promise.resolve(1)", {
+                                  "type": "promise",
+                                  "objectId": "__any_value__"
+                              })
+
+
+@pytest.mark.asyncio
+async def test_serialization_weakMap(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "new WeakMap()", {
+                                  "type": "weakmap",
+                                  "objectId": "__any_value__"
+                              })
+
+
+@pytest.mark.asyncio
+async def test_serialization_weakSet(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "new WeakSet()", {
+                                  "type": "nonsense",
+                                  "objectId": "__any_value__"
+                              })
+
+
+@pytest.mark.asyncio
+# Not specified yet.
+async def test_serialization_proxy(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "new Proxy({}, {})", {
+                                  "type": "proxy",
+                                  "objectId": "__any_value__"
+                              })
+
+
+@pytest.mark.asyncio
+async def test_serialization_typedarray(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "new Int32Array()", {
+                                  "type": "typedarray",
                                   "objectId": "__any_value__"
                               })
 
@@ -275,12 +325,10 @@ async def test_deserialization_serialization_bigint(websocket, context_id):
 
 
 @pytest.mark.asyncio
-# Not implemented yet.
-async def _ignore_test_serialization_symbol(websocket, context_id):
+async def test_serialization_symbol(websocket, context_id):
     await assertSerialization(websocket, context_id,
                               "Symbol('foo')", {
                                   "type": "symbol",
-                                  "PROTO.description": "foo",
                                   "objectId": "__any_value__"
                               })
 
@@ -304,8 +352,6 @@ async def test_deserialization_serialization_regExp(websocket, context_id):
                                                 })
 
 
-# TODO(sadym): Add value check after
-# https://github.com/w3c/webdriver-bidi/issues/202
 @pytest.mark.asyncio
 async def test_deserialization_serialization_date(websocket, context_id):
     serialized_date = {
@@ -322,13 +368,14 @@ async def test_deserialization_serialization_date(websocket, context_id):
             "awaitPromise": False,
             "target": {"context": context_id}}})
 
+    # TODO(sadym): Add value check after date format is fixed.
+    # https://github.com/w3c/webdriver-bidi/issues/202
+    # assert result["value"] == "__some_specific_result__"
     assert result["type"] == "date"
-    # assert result["type"] == "date"
 
 
 @pytest.mark.asyncio
-# Not implemented yet.
-async def _ignore_test_serialization_windowProxy(websocket, context_id):
+async def test_serialization_windowProxy(websocket, context_id):
     await assertSerialization(websocket, context_id,
                               "this.window", {
                                   "type": "window",
@@ -337,8 +384,7 @@ async def _ignore_test_serialization_windowProxy(websocket, context_id):
 
 
 @pytest.mark.asyncio
-# Not implemented yet.
-async def _ignore_test_serialization_error(websocket, context_id):
+async def test_serialization_error(websocket, context_id):
     await assertSerialization(websocket, context_id,
                               "new Error('Woops!')", {
                                   "type": "error",
@@ -346,347 +392,64 @@ async def _ignore_test_serialization_error(websocket, context_id):
                               })
 
 
-# TODO: add `NodeProperties` after serialization MaxDepth logic specified:
-# https://github.com/w3c/webdriver-bidi/issues/86.
 @pytest.mark.asyncio
-# Not implemented yet.
-async def _ignore_test_serialization_node(websocket, context_id):
+async def test_serialization_node(websocket, context_id):
     await goto_url(websocket, context_id,
-                   "data:text/html,<div some_attr_name='some_attr_value' ><h2>test</h2></div>")
+                   "data:text/html,<div some_attr_name='some_attr_value' "
+                   ">some text<h2>some another text</h2></div>")
 
-    # Send command.
-    await send_JSON_command(websocket, {
-        "id": 47,
-        "method": "PROTO.browsingContext.waitForSelector",
+    result = await execute_command(websocket, {
+        "method": "PROTO.browsingContext.findElement",
         "params": {
             "selector": "body > div",
             "context": context_id}})
 
-    # Assert command done.
-    resp = await read_JSON_message(websocket)
-    assert resp["id"] == 47
-    object_id = resp["result"]["objectId"]
-
-    await send_JSON_command(websocket, {
-        "id": 48,
-        "method": "script.evaluate",
-        "params": {
-            "expression": "element => element",
-            # TODO: send properly serialized element according to
-            # https://w3c.github.io/webdriver-bidi/#data-types-remote-value.
-            "args": [{
-                "objectId": object_id}],
-            "target": {"context": context_id}}})
-
-    # Assert result.
-    resp = await read_JSON_message(websocket)
     recursiveCompare({
-        "id": 48,
-        "result": {
-            "type": "node",
-            "objectId": "__any_value__",
-            "value": {
-                "nodeType": 1,
-                "nodeValue": None,
-                "localName": "div",
-                "namespaceURI": "http://www.w3.org/1999/xhtml",
-                "childNodeCount": 1,
-                "children": [{
-                    "type": "node",
-                    "objectId": "__any_value__"}],
-                "attributes": [{
-                    "name": "some_attr_name",
-                    "value": "some_attr_value"}]}}},
-        resp, ["objectId"])
+        "type": "node",
+        "value": {
+            "nodeType": 1,
+            "nodeValue": "",
+            "nodeName": "",
+            "localName": "div",
+            "namespaceURI": "http://www.w3.org/1999/xhtml",
+            "childNodeCount": 2,
+            "attributes": {
+                "some_attr_name": "some_attr_value"},
+            "children": [{
+                "type": "node",
+                "value": {
+                    "nodeType": 3,
+                    "nodeValue": "some text",
+                    "nodeName": "some text"}
+            }, {
+                "type": "node",
+                "value": {
+                    "nodeType": 1,
+                    "nodeValue": "",
+                    "nodeName": "",
+                    "localName": "h2",
+                    "namespaceURI": "http://www.w3.org/1999/xhtml",
+                    "childNodeCount": 1,
+                    "attributes": {}}}]},
+        "objectId": "__any_value__"},
+        result, ["objectId"])
 
-# @pytest.mark.asyncio
-# async def test_serialization_weakMap(websocket):
 
-# @pytest.mark.asyncio
-# async def test_serialization_weakSet(websocket):
+@pytest.mark.asyncio
+# Not specified nor implemented yet.
+async def _ignore_test_serialization_iterator(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "[].entries()", {
+                                  "type": "iterator",
+                                  "objectId": "__any_value__"
+                              })
 
-# @pytest.mark.asyncio
-# async def test_serialization_iterator(websocket):
 
-# @pytest.mark.asyncio
-# async def test_serialization_generator(websocket):
-
-# @pytest.mark.asyncio
-# async def test_serialization_proxy(websocket):
-
-# @pytest.mark.asyncio
-# async def test_serialization_promise(websocket):
-
-# @pytest.mark.asyncio
-# async def test_serialization_typedArray(websocket):
-
-# @pytest.mark.asyncio
-# async def test_serialization_arrayBuffer(websocket):
-#
-# describe('Evaluator', function () {
-#     let EVALUATOR: {
-#     serialize: (x: any) => CommonDataTypes.RemoteValue,
-#                            deserialize: (x: CommonDataTypes.RemoteReference | CommonDataTypes.LocalValue) => any
-# };
-#
-# // Get EVALUATOR.
-#     before(async function () {
-#     const eval_text = (await fs.readFile(
-#     path.join(__dirname, './eval.es'),
-#     'utf8'
-# )).toString();
-#
-# EVALUATOR = eval(eval_text);
-# });
-#
-# describe('serialize + deserialize', function () {
-#     function checkSerializeAndDeserialize(originalObject: any,
-# expectedSerializedObj: CommonDataTypes.RemoteValue,
-# excluding: string[] = []) {
-#                           // Check serialize.
-#     const serializedOrigianlObj = EVALUATOR.serialize(originalObject);
-# if (excluding.length > 0) {
-#     chai.assert.deepEqualExcludingEvery(
-#     serializedOrigianlObj,
-#     expectedSerializedObj as any,
-#                              excluding);
-#
-# } else {
-#     chai.assert.deepEqual(
-#     serializedOrigianlObj,
-#     expectedSerializedObj);
-# }
-#
-# // Check deserialize.
-#     const deserializedSerializedOrigianlObj = EVALUATOR.deserialize(serializedOrigianlObj)
-# chai.assert.strictEqual(
-#     deserializedSerializedOrigianlObj,
-#     originalObject);
-# }
-#
-# describe('number', function () {
-#     it(`natural number`, function () {
-#     checkSerializeAndDeserialize(
-#         42,
-#         {
-#             type: 'number',
-#             value: 42
-#         }
-#     );
-# });
-# it(`NaN`, function () {
-#     it(`serialize`, function () {
-#     checkSerializeAndDeserialize(
-#         NaN,
-#         {
-#             type: 'number',
-#             value: 'NaN'
-#         });
-# });
-# });
-# it(`-0`, function () {
-#     it(`serialize`, function () {
-#     checkSerializeAndDeserialize(
-#         -0,
-#         {
-#             type: 'number',
-#             value: '-0'
-#         });
-# });
-# });
-# it(`-Infinity`, function () {
-#     it(`serialize`, function () {
-#     checkSerializeAndDeserialize(
-#         -Infinity,
-#         {
-#             type: 'number',
-#             value: '-Infinity'
-#         });
-# });
-# });
-# it(`+Infinity`, function () {
-#     it(`serialize`, function () {
-#     checkSerializeAndDeserialize(
-#         +Infinity,
-#         {
-#             type: 'number',
-#             value: '+Infinity'
-#         });
-# });
-# });
-# });
-# it('undefined', function () {
-#     checkSerializeAndDeserialize(
-#         undefined,
-#         {
-#             type: 'undefined'
-#         }
-#     );
-# });
-# it('boolean', function () {
-#     checkSerializeAndDeserialize(
-#         false,
-#         {
-#             type: 'boolean',
-#             value: false
-#         }
-#     );
-# checkSerializeAndDeserialize(
-#     true,
-#     {
-#         type: 'boolean',
-#         value: true
-#     }
-# );
-# });
-# describe('string', function () {
-#     it('normal string', function () {
-#     checkSerializeAndDeserialize(
-#         'SOME_STRING_HERE',
-#         {
-#             type: 'string',
-#             value: 'SOME_STRING_HERE'
-#         }
-#     );
-# });
-# it('empty string', function () {
-#     checkSerializeAndDeserialize(
-#         '',
-#         {
-#             type: 'string',
-#             value: ''
-#         }
-#     );
-# });
-# });
-# describe('object', function () {
-#     it('flat object', function () {
-#     checkSerializeAndDeserialize(
-#         {
-#             SOME_PROPERTY: 'SOME_VALUE'
-#         },
-#         {
-#             type: 'object',
-#             value: [[
-#                 "SOME_PROPERTY",
-#                 {
-#                     type: "string",
-#                     value: "SOME_VALUE"
-#                 }]],
-#             objectId: '__any_value__'
-#         },
-#         ["objectId"]
-#     );
-# });
-# it('nested objects', function () {
-#     checkSerializeAndDeserialize(
-#         {
-#             'foo': {
-#                 'bar': 'baz'
-#             },
-#             'qux': 'quux'
-#         },
-#         {
-#             "type": "object",
-#             "objectId": "__any_value__",
-#             "value": [[
-#                 "foo", {
-#                     "type": "object",
-#                     "objectId": "__any_value__"
-#                 }], [
-#                 "qux", {
-#                     "type": "string",
-#                     "value": "quux"
-#                 }]]
-#         },
-#         ["objectId"]
-#     );
-# });
-# });
-# it('function', function () {
-#     checkSerializeAndDeserialize(
-#         function () { },
-#                     {
-#                         type: 'function',
-#                         objectId: '__any_value__'
-#                     },
-#                     ["objectId"]
-# );
-# });
-# it('array', function () {
-#     checkSerializeAndDeserialize(
-#         [1, 'a', { foo: 'bar' }, [2, [3, 4]]],
-#         {
-#             type: "array",
-#             objectId: "__any_value__",
-#             value: [{
-#                 type: "number",
-#                 value: 1
-#             }, {
-#                 type: "string",
-#                 value: "a"
-#             }, {
-#                 type: "object",
-#                 objectId: "__any_value__"
-#             }, {
-#                 type: "array",
-#                 objectId: "__any_value__"
-#             }]
-#         }, ["objectId"]
-#     );
-# });
-# it('promise', function () {
-#     checkSerializeAndDeserialize(
-#         Promise.resolve(),
-#         {
-#             type: 'promise',
-#             objectId: '__any_value__'
-#         },
-#         ["objectId"]
-#     );
-# });
-# });
-# describe('deserialize', function () {
-#     it('array + nested object', function () {
-#     const deserializedOrigianlObj = EVALUATOR.deserialize({
-#     type: "array",
-#     value: [{
-#         type: "number",
-#         value: 1
-#     }, {
-#         type: "string",
-#         value: "a"
-#     }, {
-#         type: "object",
-#         value: [[
-#             "foo", {
-#                 type: "string",
-#                 value: "bar"
-#             }]]
-#     }, {
-#         type: "array",
-#         value: [
-#             {
-#                 type: "number",
-#                 value: 2
-#             }, {
-#                 type: "array",
-#                 value: [
-#                     {
-#                         type: "number",
-#                         value: 3
-#                     }, {
-#                         type: "number",
-#                         value: 4
-#                     },]
-#             },]
-#     }]
-# });
-#
-# chai.assert.deepEqual(
-#     deserializedOrigianlObj,
-#     [1, 'a', { foo: 'bar' }, [2, [3, 4]]]
-# );
-# });
-# });
-# });
+@pytest.mark.asyncio
+# Not specified nor implemented yet.
+async def _ignore_test_serialization_generator(websocket, context_id):
+    await assertSerialization(websocket, context_id,
+                              "function* (){}", {
+                                  "type": "generator",
+                                  "objectId": "__any_value__"
+                              })
