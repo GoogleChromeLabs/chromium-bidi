b2b1a7747dd81766d464183e0bfaff1826d65a75
Thiago Perrotta <tperrotta@chromium.org>
2023-03-02 11:47:31 -0500

    make session.unsubscribe command atomic
    If an operation failed for some reason (not subscribed, browsing context
    not found, etc), do not unsubscribe partially.
    
    Closes #482

--- src/bidiMapper/domains/events/SubscriptionManager.ts
+++ src/bidiMapper/domains/events/SubscriptionManager.ts
@@ -26,6 +26,43 @@ import {
 import {BrowsingContextStorage} from '../context/browsingContextStorage.js';
 import InvalidArgumentException = Message.InvalidArgumentException;
 
+/**
+ * Returns the cartesian product of the given arrays.
+ *
+ * Example:
+ *   cartesian([1, 2], ['a', 'b']); => [[1, 'a'], [1, 'b'], [2, 'a'], [2, 'b']]
+ */
+function cartesianProduct(...a: any[][]) {
+  return a.reduce((a: unknown[], b: unknown[]) =>
+    a.flatMap((d: unknown) => b.map((e: unknown) => [d, e].flat()))
+  );
+}
+
+/** Expands "AllEvents" events into atomic events. */
+function unrollEvents(
+  events: Session.SubscribeParametersEvent[]
+): Session.SubscribeParametersEvent[] {
+  const allEvents: Session.SubscribeParametersEvent[] = [];
+
+  for (const event of events) {
+    switch (event) {
+      case BrowsingContext.AllEvents:
+        allEvents.push(...Object.values(BrowsingContext.EventNames));
+        break;
+      case CDP.AllEvents:
+        allEvents.push(...Object.values(CDP.EventNames));
+        break;
+      case Log.AllEvents:
+        allEvents.push(...Object.values(Log.EventNames));
+        break;
+      default:
+        allEvents.push(event);
+    }
+  }
+
+  return allEvents;
+}
+
 export class SubscriptionManager {
   #subscriptionPriority = 0;
   // BrowsingContext `null` means the event has subscription across all the
@@ -158,32 +195,61 @@ export class SubscriptionManager {
     eventMap.set(event, this.#subscriptionPriority++);
   }
 
-  unsubscribe(
+  /**
+   * Unsubscribes atomically from all events in the given contexts and channel.
+   */
+  async unsubscribeAll(
+    eventNames: Session.SubscribeParametersEvent[],
+    contextIds: (CommonDataTypes.BrowsingContext | null)[],
+    channel: string | null
+  ): Promise<void> {
+    // Assert all contexts are known.
+    for (const contextId of contextIds) {
+      if (contextId !== null) {
+        this.#browsingContextStorage.getKnownContext(contextId);
+      }
+    }
+
+    const eventContextPairs: Array<
+      [
+        eventName: Session.SubscribeParametersEvent,
+        contextId: CommonDataTypes.BrowsingContext | null
+      ]
+    > = cartesianProduct(unrollEvents(eventNames), contextIds);
+
+    // Assert all unsubscriptions are valid.
+    // If any of the unsubscriptions are invalid, do not unsubscribe from anything.
+    const unsubscribes: Array<Function> = await Promise.all(
+      eventContextPairs.map(([eventName, contextId]) =>
+        this.#checkUnsubscribe(eventName, contextId, channel)
+      )
+    );
+
+    // Unsubscribe from all.
+    for (const unsubscribe of unsubscribes) {
+      unsubscribe();
+    }
+  }
+
+  /**
+   * Unsubscribes from the event in the given context and channel.
+   * Syntactic sugar for "unsubscribeAll".
+   */
+  async unsubscribe(
+    eventName: Session.SubscribeParametersEvent,
+    contextId: CommonDataTypes.BrowsingContext | null,
+    channel: string | null
+  ): Promise<void> {
+    return this.unsubscribeAll([eventName], [contextId], channel);
+  }
+
+  async #checkUnsubscribe(
     event: Session.SubscribeParametersEvent,
     contextId: CommonDataTypes.BrowsingContext | null,
     channel: string | null
-  ): void {
+  ): Promise<Function> {
     // All the subscriptions are handled on the top-level contexts.
     contextId = this.#findTopLevelContextId(contextId);
-
-    if (event === BrowsingContext.AllEvents) {
-      Object.values(BrowsingContext.EventNames).map((specificEvent) =>
-        this.unsubscribe(specificEvent, contextId, channel)
-      );
-      return;
-    }
-    if (event === CDP.AllEvents) {
-      Object.values(CDP.EventNames).map((specificEvent) =>
-        this.unsubscribe(specificEvent, contextId, channel)
-      );
-      return;
-    }
-    if (event === Log.AllEvents) {
-      Object.values(Log.EventNames).map((specificEvent) =>
-        this.unsubscribe(specificEvent, contextId, channel)
-      );
-      return;
-    }
 
     if (!this.#channelToContextToEventMap.has(channel)) {
       throw new InvalidArgumentException(
@@ -205,14 +271,16 @@ export class SubscriptionManager {
       );
     }
 
-    eventMap.delete(event);
+    return () => {
+      eventMap.delete(event);
 
-    // Clean up maps if empty.
-    if (eventMap.size === 0) {
-      contextToEventMap.delete(event);
-    }
-    if (contextToEventMap.size === 0) {
-      this.#channelToContextToEventMap.delete(channel);
-    }
+      // Clean up maps if empty.
+      if (eventMap.size === 0) {
+        contextToEventMap.delete(event);
+      }
+      if (contextToEventMap.size === 0) {
+        this.#channelToContextToEventMap.delete(channel);
+      }
+    };
   }
 }
