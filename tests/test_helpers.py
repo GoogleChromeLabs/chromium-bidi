3138259db98430cd009192dc91fa6b7a4bdf6dd3
Alex Rudenko <alexrudenko@chromium.org>
2023-05-03 16:00:21 +0200

    up

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -16,8 +16,6 @@ from __future__ import annotations
 
 import itertools
 import json
-import sys
-import traceback
 from typing import Literal
 
 from anys import ANY_NUMBER, ANY_STR, AnyContains, AnyGT, AnyLT, AnyWithEntries
@@ -88,20 +86,13 @@ async def execute_command(websocket, command: dict) -> dict:
 
 async def get_tree(websocket, context_id: str | None = None) -> dict:
     """Get the tree of browsing contexts."""
-    print('XXX get_tree was called: BEFORE')
-    traceback.print_tb(sys.exc_info()[2])
-
     params = {}
     if context_id is not None:
         params["root"] = context_id
-    get_tree_result = await execute_command(websocket, {
+    return await execute_command(websocket, {
         "method": "browsingContext.getTree",
         "params": params
     })
-
-    print('XXX get_tree was called: AFTER')
-
-    return get_tree_result
 
 
 async def goto_url(
