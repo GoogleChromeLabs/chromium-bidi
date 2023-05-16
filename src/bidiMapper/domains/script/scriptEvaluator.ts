30d4521789a40c1e56f7ab3b80ab64562c6026f0
Maksim Sadym <69349599+sadym-chromium@users.noreply.github.com>
2023-05-16 14:39:42 +0200

    Update src/bidiMapper/domains/script/scriptEvaluator.ts
    Co-authored-by: Thiago Perrotta <tperrotta@chromium.org>

--- src/bidiMapper/domains/script/scriptEvaluator.ts
+++ src/bidiMapper/domains/script/scriptEvaluator.ts
@@ -144,7 +144,7 @@ export class ScriptEvaluator {
     serializationOptions: Script.SerializationOptions
   ): Promise<Script.ScriptResult> {
     if (![0, null, undefined].includes(serializationOptions.maxDomDepth))
-      throw new Error('serializationOptions.maxDomDepth!=0 is not supported');
+      throw new Error('serializationOptions.maxDomDepth other than 0 or null is not supported');
 
     const callFunctionAndSerializeScript = `(...args)=>{ return _callFunction((\n${functionDeclaration}\n), args);
       function _callFunction(f, args) {
