3eaaf34d6a91b7c97beca6b467aa80cf26920e71
Nikolay Vitkov <nvitkov@chromium.org>
2023-06-12 11:33:23 +0200

    chore: fixes

--- tests/test_browsing_context.py
+++ tests/test_browsing_context.py
@@ -874,8 +874,7 @@ async def test_browsingContext_reload_waitComplete(websocket, context_id,
 @pytest.mark.parametrize("ignoreCache", [True, False])
 async def test_browsingContext_ignoreCache(websocket, context_id, ignoreCache):
     if not ignoreCache:
-        pytest.xfail(
-            reason="TODO: Fix flakiness with ignoreCache=False")
+        pytest.xfail(reason="TODO: Fix flakiness with ignoreCache=False")
         
     url = "https://example.com/"
 
@@ -917,7 +916,7 @@ async def test_browsingContext_ignoreCache(websocket, context_id, ignoreCache):
             "navigation": ANY_STR,
             "redirectCount": 0,
             "request": ANY_DICT,
-            "response": AnyExtending({"status": 200 if ignoreCache else 304 }),
+            "response": AnyExtending({"status": 200 if ignoreCache else 304}),
             "timestamp": ANY_TIMESTAMP,
         },
     }
