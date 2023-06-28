b6dce04f6bc9af04d088e6a4a51b990f6a1807b6
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 13:38:38 +0200

    chore: merge my local updateWPT.sh with runWPTAll.sh

--- runWPTAll.sh
+++ runWPTAll.sh
@@ -2,6 +2,26 @@
 # shellcheck disable=SC2155
 
 set -uo pipefail
-HEADLESS=false exec npm run wpt -- "$@"
-HEADLESS=true exec npm run wpt -- "$@"
-CHROMEDRIVER=true exec npm run wpt -- "$@"
+
+trap ctrl_c INT
+ctrl_c() {
+    exit 1
+}
+
+export LOG_FILE="logs/$(basename "$0").log"
+export UPDATE_EXPECTATIONS="${UPDATE_EXPECTATIONS:-false}"
+
+for test in "$@"; do
+    test="${test//wpt-metadata\//}"
+    test="${test//mapper\/headless\//}"
+    test="${test//mapper\/headful\//}"
+    test="${test//chromedriver\/headless\//}"
+    test="${test//.ini/}"
+
+    CHROMEDRIVER=true npm run wpt -- "$test"
+    HEADLESS=false npm run wpt -- "$test"
+    HEADLESS=true npm run wpt -- "$test"
+done
+
+# Grep for errors if $LOG is set.
+[[ -n "$LOG" ]] && [[ -r "$LOG_FILE" ]] && less -p error "$LOG_FILE"
