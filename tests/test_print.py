2be00759be6574efd24d4f34df90e4a2f14b0312
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 17:41:03 +0200

    test: compare similarity of print to pdf output
    Bug: #518

--- tests/test_print.py
+++ tests/test_print.py
@@ -14,10 +14,15 @@
 # limitations under the License.
 
 import base64
+from difflib import SequenceMatcher
 
 import pytest
-from anys import ANY_INT, ANY_STR
-from test_helpers import goto_url, read_JSON_message, send_JSON_command
+from test_helpers import execute_command, goto_url
+
+
+def assert_strings_similar(str1: str, str2: str, threshold: float):
+    similarity_ratio = SequenceMatcher(None, str1, str2).ratio()
+    assert similarity_ratio >= threshold, "Strings are not similar enough."
 
 
 def save_pdf(pdf_bytes_or_str: bytes | str, output_file: str):
@@ -32,27 +37,24 @@ def save_pdf(pdf_bytes_or_str: bytes | str, output_file: str):
 
 
 @pytest.mark.asyncio
-async def test_print(websocket, context_id):
-    await goto_url(websocket, context_id, 'about:blank')
+async def test_print(websocket, context_id, html):
+    await goto_url(websocket, context_id, html())
 
-    await send_JSON_command(
+    result = await execute_command(
         websocket, {
             "method": "browsingContext.print",
             "params": {
                 "context": context_id,
-                "background": False,
-                "orientation": "portrait",
                 "page": {
-                    "width": 800,
-                    "height": 600,
+                    "width": 100,
+                    "height": 100,
                 },
-                "pageRanges": ["1-"],
                 "scale": 1.0,
             }
         })
 
-    resp = await read_JSON_message(websocket)
-
-    # 'data' is not deterministic.
-    # There is always ~half a dozen characters that differ between runs.
-    assert resp == {'id': ANY_INT, 'result': {'data': ANY_STR}}
+    # 'data' is not deterministic, ~a dozen characters differ between runs.
+    assert_strings_similar(
+        result["data"],
+        'JVBERi0xLjQKJdPr6eEKMSAwIG9iago8PC9DcmVhdG9yIChDaHJvbWl1bSkKL1Byb2R1Y2VyIChTa2lhL1BERiBtMTE3KQovQ3JlYXRpb25EYXRlIChEOjIwMjMwNjI4MTUzMTQ3KzAwJzAwJykKL01vZERhdGUgKEQ6MjAyMzA2MjgxNTMxNDcrMDAnMDAnKT4+CmVuZG9iagozIDAgb2JqCjw8L0xlbmd0aCAwPj4gc3RyZWFtCgplbmRzdHJlYW0KZW5kb2JqCjIgMCBvYmoKPDwvVHlwZSAvUGFnZQovUmVzb3VyY2VzIDw8L1Byb2NTZXQgWy9QREYgL1RleHQgL0ltYWdlQiAvSW1hZ2VDIC9JbWFnZUldPj4KL01lZGlhQm94IFswIDAgOTA3LjkxOTk4IDkwNy45MTk5OF0KL0NvbnRlbnRzIDMgMCBSCi9TdHJ1Y3RQYXJlbnRzIDAKL1BhcmVudCA0IDAgUj4+CmVuZG9iago0IDAgb2JqCjw8L1R5cGUgL1BhZ2VzCi9Db3VudCAxCi9LaWRzIFsyIDAgUl0+PgplbmRvYmoKNSAwIG9iago8PC9UeXBlIC9DYXRhbG9nCi9QYWdlcyA0IDAgUj4+CmVuZG9iagp4cmVmCjAgNgowMDAwMDAwMDAwIDY1NTM1IGYgCjAwMDAwMDAwMTUgMDAwMDAgbiAKMDAwMDAwMDIwMiAwMDAwMCBuIAowMDAwMDAwMTU1IDAwMDAwIG4gCjAwMDAwMDAzNzcgMDAwMDAgbiAKMDAwMDAwMDQzMiAwMDAwMCBuIAp0cmFpbGVyCjw8L1NpemUgNgovUm9vdCA1IDAgUgovSW5mbyAxIDAgUj4+CnN0YXJ0eHJlZgo0NzkKJSVFT0YK',
+        threshold=0.95)
