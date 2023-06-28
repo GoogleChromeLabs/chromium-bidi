d9475d80b4537e6113a5bdf2c53d9061f0677911
jrandolf <101637635+jrandolf@users.noreply.github.com>
2023-06-28 10:21:55 +0200

    Update runWPTAll.sh
    Co-authored-by: Alex Rudenko <OrKoN@users.noreply.github.com>

--- runWPTAll.sh
+++ runWPTAll.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# shellcheck disable=SC2155
+
+set -uo pipefail
 HEADLESS=false exec npm run wpt -- "$@"
 HEADLESS=true exec npm run wpt -- "$@"
 CHROMEDRIVER=true exec npm run wpt -- "$@"
