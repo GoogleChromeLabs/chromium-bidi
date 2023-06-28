4f5b2e59cb98f101c9a0c6d343657c9b4def3ede
Thiago Perrotta <tperrotta@chromium.org>
2023-06-28 13:41:50 +0200

    chore: make runWPTAll.sh work with multiple arguments

--- runWPTAll.sh
+++ runWPTAll.sh
@@ -11,17 +11,23 @@ ctrl_c() {
 export LOG_FILE="logs/$(basename "$0").log"
 export UPDATE_EXPECTATIONS="${UPDATE_EXPECTATIONS:-false}"
 
-for test in "$@"; do
-    test="${test//wpt-metadata\//}"
-    test="${test//mapper\/headless\//}"
-    test="${test//mapper\/headful\//}"
-    test="${test//chromedriver\/headless\//}"
-    test="${test//.ini/}"
+if [[ -n "$*" ]]; then
+    for test in "$@"; do
+        test="${test//wpt-metadata\//}"
+        test="${test//mapper\/headless\//}"
+        test="${test//mapper\/headful\//}"
+        test="${test//chromedriver\/headless\//}"
+        test="${test//.ini/}"
 
-    CHROMEDRIVER=true npm run wpt -- "$test"
-    HEADLESS=false npm run wpt -- "$test"
-    HEADLESS=true npm run wpt -- "$test"
-done
+        CHROMEDRIVER=true npm run wpt -- "$test"
+        HEADLESS=false npm run wpt -- "$test"
+        HEADLESS=true npm run wpt -- "$test"
+    done
+else
+    CHROMEDRIVER=true npm run wpt
+    HEADLESS=false npm run wpt
+    HEADLESS=true npm run wpt
+fi
 
 # Grep for errors if $LOG is set.
 [[ -n "$LOG" ]] && [[ -r "$LOG_FILE" ]] && less -p error "$LOG_FILE"
