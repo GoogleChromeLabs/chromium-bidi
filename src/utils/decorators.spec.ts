14396e9500765181249706e6b8bbd1d808e88636
Randolf Jung <jrandolf@chromium.org>
2023-08-07 02:55:58 +0200

    chore: implement injection framework

---
+++ src/utils/decorators.spec.ts
@@ -0,0 +1,509 @@
+/**
+ * Copyright 2023 Google LLC.
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as chai from 'chai';
+
+import {feed, eat, pantry} from './decorators.js';
+
+const expect = chai.expect;
+
+const SymbolSym = Symbol('symbol');
+const SymbolSym2 = Symbol('symbol2');
+
+declare module './decorators.js' {
+  interface InjectableRegistry {
+    [SymbolSym]: symbol;
+    [SymbolSym2]: symbol;
+  }
+}
+
+describe('Decorators', () => {
+  describe('inject: field, provides: field, consumes: field', () => {
+    it('should work', () => {
+      class A {
+        @pantry(SymbolSym)
+        readonly value = Symbol();
+
+        @feed([SymbolSym])
+        readonly b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        readonly value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should work out of order', () => {
+      class A {
+        @feed([SymbolSym])
+        readonly b = new B();
+
+        @pantry(SymbolSym)
+        readonly value = Symbol();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        readonly value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should not work with constructors', () => {
+      class A {
+        @pantry(SymbolSym)
+        readonly value: symbol;
+
+        @feed([SymbolSym])
+        readonly b: B;
+
+        constructor() {
+          this.value = Symbol();
+          this.b = new B();
+        }
+      }
+
+      class B {
+        @eat(SymbolSym)
+        readonly value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(undefined);
+    });
+
+    it('should not descend modifications', () => {
+      class A {
+        @pantry(SymbolSym)
+        readonly value = Symbol();
+
+        @feed([SymbolSym])
+        readonly b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        readonly value!: symbol;
+      }
+
+      const a = new A();
+      // @ts-expect-error We are trying to do something bad here.
+      a.value = Symbol();
+      expect(a.b.value).not.equals(a.value);
+    });
+
+    it('should not reinject', () => {
+      class A {
+        @pantry(SymbolSym)
+        readonly value = Symbol();
+
+        @feed([SymbolSym])
+        readonly b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        readonly value!: symbol;
+      }
+
+      const a = new A();
+      // @ts-expect-error We are trying to do something bad here.
+      a.b = new B();
+      expect(a.b.value).equals(undefined);
+    });
+  });
+
+  describe('inject: setter/getter, provides: getter, consumes: setter', () => {
+    it('should work with injected setters', () => {
+      class A {
+        @pantry(Number)
+        get value() {
+          return 5;
+        }
+
+        internalB?: B;
+        @feed([Number])
+        set b(value: B) {
+          this.internalB = value;
+        }
+      }
+
+      class B {
+        internalValue?: number;
+        @eat(Number)
+        set value(value: number) {
+          this.internalValue = value;
+        }
+      }
+
+      const a = new A();
+      expect(a.internalB).equals(undefined);
+
+      const b = new B();
+      expect(b.internalValue).equals(undefined);
+
+      a.b = b;
+      expect(a.internalB).equals(b);
+      expect(b.internalValue).equals(a.value);
+    });
+
+    it('should work with injected getters', () => {
+      class A {
+        @pantry(Number)
+        get value() {
+          return 5;
+        }
+
+        @feed([Number])
+        get b() {
+          return new B();
+        }
+      }
+
+      class B {
+        internalValue?: number;
+        @eat(Number)
+        set value(value: number) {
+          this.internalValue = value;
+        }
+      }
+
+      const a = new A();
+      expect(a.b.internalValue).equals(a.value);
+    });
+  });
+
+  describe('inject: accessor, provides: accessor, consumes: accessor', () => {
+    it('should work', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+
+        @feed([SymbolSym])
+        accessor b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should work out of order', () => {
+      class A {
+        @feed([SymbolSym])
+        accessor b = new B();
+
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should work with constructors', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value: symbol;
+
+        @feed([SymbolSym])
+        accessor b: B;
+
+        constructor() {
+          this.value = Symbol();
+          this.b = new B();
+        }
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should descend modifications', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+
+        @feed([SymbolSym])
+        accessor b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      a.value = Symbol();
+      expect(a.b.value).equals(a.value);
+    });
+
+    it('should reinject', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+
+        @feed([SymbolSym])
+        accessor b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      a.b = new B();
+      expect(a.b.value).equals(a.value);
+    });
+  });
+
+  describe('inject: field, provides: accessor, consumes: accessor', () => {
+    it('should not work out of order in constructor', () => {
+      class A {
+        @feed([SymbolSym])
+        accessor b: B;
+
+        @pantry(SymbolSym)
+        readonly value: symbol;
+
+        constructor() {
+          this.b = new B();
+          this.value = Symbol();
+        }
+      }
+
+      class B {
+        @eat(SymbolSym)
+        accessor value!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.value).equals(undefined);
+    });
+  });
+
+  it('should work with multiple', () => {
+    class A {
+      @pantry(SymbolSym)
+      accessor value = Symbol();
+      @pantry(SymbolSym2)
+      accessor value2 = Symbol();
+
+      @feed([SymbolSym, SymbolSym2])
+      accessor b = new B();
+    }
+
+    class B {
+      @eat(SymbolSym)
+      accessor value!: symbol;
+      @eat(SymbolSym2)
+      accessor value2!: symbol;
+    }
+
+    const a = new A();
+    expect(a.b.value).equals(a.value);
+    expect(a.b.value2).equals(a.value2);
+  });
+
+  it('should work with private properties', () => {
+    class A {
+      @pantry(SymbolSym)
+      accessor #value = Symbol();
+
+      get value(): symbol {
+        return this.#value;
+      }
+
+      @feed([SymbolSym])
+      accessor #b = new B();
+
+      get b(): B {
+        return this.#b;
+      }
+    }
+
+    class B {
+      @eat(SymbolSym)
+      accessor #value!: symbol;
+
+      get value(): symbol {
+        return this.#value;
+      }
+    }
+
+    const a = new A();
+    expect(a.b.value).equals(a.value);
+  });
+
+  it('should work with consumer setter', () => {
+    class A {
+      @pantry(SymbolSym)
+      accessor value = Symbol();
+
+      @feed([SymbolSym])
+      accessor b = new B();
+    }
+
+    class B {
+      @eat(SymbolSym)
+      set value(value: symbol) {
+        this.value2 = value;
+      }
+
+      value2!: symbol;
+    }
+
+    const a = new A();
+    expect(a.b.value2).equals(a.value);
+  });
+
+  it('should work not have properties before constructor is used', () => {
+    class A {
+      @pantry(SymbolSym)
+      accessor value = Symbol();
+
+      @feed([SymbolSym])
+      accessor b = new B();
+    }
+
+    class B {
+      @eat(SymbolSym)
+      accessor value!: symbol;
+
+      accessor value2: symbol;
+
+      constructor() {
+        this.value2 = this.value;
+      }
+    }
+
+    const a = new A();
+    expect(a.b.value2).equals(undefined);
+  });
+
+  describe('nesting', () => {
+    it('should work', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+
+        @feed([SymbolSym])
+        accessor b = new B();
+      }
+
+      class B {
+        @eat(SymbolSym)
+        @pantry(SymbolSym)
+        accessor bValue!: symbol;
+
+        @feed([SymbolSym])
+        accessor c = new C();
+      }
+
+      class C {
+        @eat(SymbolSym)
+        accessor cValue!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.bValue).equals(a.value);
+      expect(a.b.c.cValue).equals(a.value);
+
+      a.value = Symbol();
+      expect(a.b.bValue).equals(a.value);
+      expect(a.b.c.cValue).equals(a.value);
+    });
+
+    it('should work deeply', () => {
+      class A {
+        @pantry(SymbolSym)
+        accessor value = Symbol();
+
+        @feed([SymbolSym])
+        accessor b = new B();
+      }
+
+      class B {
+        @feed([SymbolSym])
+        accessor c = new C();
+      }
+
+      class C {
+        @feed([SymbolSym])
+        accessor d = new D();
+      }
+
+      class D {
+        @eat(SymbolSym)
+        accessor dValue!: symbol;
+      }
+
+      const a = new A();
+      expect(a.b.c.d.dValue).equals(a.value);
+
+      a.value = Symbol();
+      expect(a.b.c.d.dValue).equals(a.value);
+    });
+  });
+
+  it('should work with multiple consumers', () => {
+    class A {
+      @pantry(SymbolSym)
+      accessor value = Symbol();
+
+      @feed([SymbolSym])
+      accessor b = new B();
+    }
+
+    class B {
+      @eat(SymbolSym)
+      accessor bValue!: symbol;
+
+      @eat(SymbolSym)
+      accessor bValue2!: symbol;
+    }
+
+    const a = new A();
+    expect(a.b.bValue).equals(a.value);
+    expect(a.b.bValue2).equals(a.value);
+
+    a.value = Symbol();
+    expect(a.b.bValue).equals(a.value);
+    expect(a.b.bValue2).equals(a.value);
+  });
+});
---
+++ src/utils/decorators.ts
@@ -0,0 +1,352 @@
+/**
+ * Copyright 2023 Google LLC.
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+type ClassMemberDecoratorContext<This, Value> =
+  | ClassGetterDecoratorContext<This, Value>
+  | ClassSetterDecoratorContext<This, Value>
+  | ClassFieldDecoratorContext<This, Value>
+  | ClassAccessorDecoratorContext<This, Value>;
+
+type InjectableKey =
+  | (abstract new (...args: any[]) => any)
+  | keyof InjectableRegistry;
+
+type ValueOf<Marker extends InjectableKey> =
+  Marker extends keyof InjectableRegistry
+    ? InjectableRegistry[Marker]
+    : Marker extends abstract new (...args: any[]) => any
+    ? InstanceType<Marker>
+    : never;
+
+/**
+ * Augment this to register your injectables.
+ *
+ * For example,
+ *
+ * ```ts
+ * const LoggerSym = Symbol('symbol');
+ *
+ * declare module './path/to/decorators.js' {
+ *  interface InjectableRegistry {
+ *    [LoggerSym]: Logger;
+ *   }
+ * }
+ * ```
+ */
+export interface InjectableRegistry {}
+
+type Consumer = {
+  getters: Map<symbol | (abstract new (...args: any[]) => any), () => any>;
+  fields: Map<string | symbol, object>;
+};
+
+type Provider = Map<
+  symbol | (abstract new (...args: any[]) => any),
+  ((value: any) => void)[]
+>;
+
+const STATES = new WeakMap<
+  abstract new (...args: unknown[]) => unknown,
+  {
+    consumers?: WeakMap<object, Consumer>;
+    providers?: WeakMap<object, Provider>;
+  }
+>();
+
+function getInjectorState(object: object) {
+  const Class = object.constructor as abstract new (
+    ...args: unknown[]
+  ) => unknown;
+  let state = STATES.get(Class);
+  if (!state) {
+    state = {};
+    STATES.set(Class, state);
+  }
+  return state;
+}
+
+function getOrCreateProvider(object: object) {
+  const state = getInjectorState(object);
+  state.providers ??= new WeakMap();
+
+  const providers = state.providers;
+  let provider = providers.get(object);
+  if (!provider) {
+    provider = new Map();
+    providers.set(object, provider);
+  }
+  return provider;
+}
+
+function getProvider(object: object) {
+  const state = getInjectorState(object);
+  state.providers ??= new WeakMap();
+
+  const providers = state.providers;
+  return providers.get(object);
+}
+
+function getOrCreateConsumers(object: object) {
+  const state = getInjectorState(object);
+  state.consumers ??= new WeakMap();
+
+  const consumers = state.consumers;
+  let consumer = consumers.get(object);
+  if (!consumer) {
+    consumer = {getters: new Map(), fields: new Map()};
+    consumers.set(object, consumer);
+  }
+  return consumer;
+}
+
+/**
+ * Injects the decorated member with objects associated with the given symbols.
+ *
+ * Before injecting a given symbol, make sure it's registered with
+ * {@link InjectableRegistry}.
+ */
+export function feed(marks: InjectableKey[]) {
+  return (
+    target: any,
+    context: ClassMemberDecoratorContext<object, object>
+  ): any => {
+    let consumers: Consumer;
+    context.addInitializer(function initializer() {
+      consumers = getOrCreateConsumers(this);
+
+      const provider = getOrCreateProvider(this);
+      for (const mark of marks) {
+        const setters = provider.get(mark) ?? [];
+        setters.push(reinject.bind(undefined, consumers, mark));
+        provider.set(mark, setters);
+      }
+    });
+
+    switch (context.kind) {
+      case 'accessor':
+        return {
+          set(this: object, value?: object) {
+            return target.set.call(this, inject(value));
+          },
+          init: inject,
+        };
+      case 'field':
+        return inject;
+      case 'setter':
+        return function set(this: object, value?: object) {
+          return target.call(this, inject(value));
+        };
+      case 'getter':
+        return function get(this: object) {
+          return inject(target.call(this));
+        };
+    }
+
+    function inject(object?: object) {
+      if (object !== undefined) {
+        const provider = getProvider(object);
+        if (provider !== undefined) {
+          consumers.fields.set(context.name, object);
+          for (const mark of marks) {
+            const get = consumers.getters.get(mark);
+            if (get !== undefined) {
+              const setters = provider.get(mark)!;
+              if (setters !== undefined) {
+                for (const set of setters) {
+                  set(get());
+                }
+              }
+            }
+          }
+        }
+      }
+      return object;
+    }
+  };
+}
+
+/**
+ * Declares a member as an object provider for the given symbol.
+ *
+ * Before injecting a given symbol, make sure it's registered with
+ * {@link InjectableRegistry}.
+ */
+export function pantry<Marker extends InjectableKey>(mark: Marker) {
+  return <
+    Interface,
+    Value = ValueOf<Marker> extends Interface ? Interface : ValueOf<Marker>,
+  >(
+    target: any,
+    context: ClassMemberDecoratorContext<object, Value>
+  ): any => {
+    let consumers: Consumer;
+    context.addInitializer(function initializer() {
+      consumers = getOrCreateConsumers(this);
+      switch (context.kind) {
+        case 'getter':
+          consumers.getters.set(mark, context.access.get.bind(undefined, this));
+          break;
+        default:
+      }
+    });
+
+    switch (context.kind) {
+      case 'accessor':
+        return {
+          set(this: object, value: Value) {
+            return target.set.call(this, reinject(consumers, mark, value));
+          },
+          init(this: object, initialValue: Value) {
+            consumers.getters.set(
+              mark,
+              context.access.get.bind(undefined, this)
+            );
+            return reinject(consumers, mark, initialValue);
+          },
+        };
+      case 'field':
+        return function init(this: object, initialValue: Value) {
+          consumers.getters.set(mark, context.access.get.bind(undefined, this));
+          return reinject(consumers, mark, initialValue);
+        };
+      case 'getter':
+        break;
+      default:
+        throw new Error(`Cannot decorate ${context.kind}`);
+    }
+  };
+}
+
+/**
+ * Declares a member as an object consumer for the given symbol.
+ *
+ * Before injecting a given symbol, make sure it's registered with
+ * {@link InjectableRegistry}.
+ */
+export function eat<Marker extends InjectableKey>(mark: Marker) {
+  return <
+    Interface,
+    Value = ValueOf<Marker> extends Interface ? Interface : ValueOf<Marker>,
+  >(
+    _: unknown,
+    context: ClassMemberDecoratorContext<object, Value>
+  ): any => {
+    context.addInitializer(function initializer() {
+      const provider = getOrCreateProvider(this);
+      switch (context.kind) {
+        case 'field':
+        case 'setter':
+        case 'accessor': {
+          const setters = provider.get(mark) ?? [];
+          setters.push(context.access.set.bind(undefined, this));
+          provider.set(mark, setters);
+          break;
+        }
+        default:
+          throw new Error(`Cannot decorate ${context.kind}`);
+      }
+    });
+  };
+}
+
+/**
+ * For testing only! Injects a given object with a provided set of injectables.
+ *
+ * This must not be used to "skip" constructor arguments in favor of injection.
+ *
+ * For example, if you need to inject a `Logger` module into some class, do
+ *
+ * ```ts
+ * class A {
+ *  @pantry(LoggerSym)
+ *  #logger: Logger
+ *
+ *  constructor(logger: Logger) {
+ *    this.#logger = logger;
+ *  }
+ * }
+ *
+ * const a = new A(new Logger());
+ * ```
+ *
+ * Do not do
+ *
+ * ```ts
+ * class A {
+ *  @eat(LoggerSym)
+ *  #logger: Logger
+ *
+ *  constructor(logger: Logger) {
+ *    this.#logger = logger;
+ *  }
+ * }
+ *
+ * const a = inject(new A());
+ * ```
+ *
+ * If you need to pass some object to some descendent, you should declare
+ * the object on the root class, even if it's not used by the root class.
+ *
+ */
+export function inject<
+  T extends object,
+  Injectables extends Partial<InjectableRegistry> | Set<object>,
+>(object: T, injectables: Injectables): T {
+  const provider = getProvider(object);
+  if (provider !== undefined) {
+    if (injectables instanceof Set) {
+      for (const object of injectables) {
+        const setters = provider.get(object.constructor);
+        if (setters !== undefined) {
+          for (const set of setters) {
+            set(object);
+          }
+        }
+      }
+    } else {
+      for (const mark of Object.getOwnPropertySymbols(
+        injectables
+      ) as (keyof InjectableRegistry)[]) {
+        const setters = provider.get(mark);
+        if (setters !== undefined) {
+          for (const set of setters) {
+            set(injectables[mark]);
+          }
+        }
+      }
+    }
+  }
+  return object;
+}
+
+function reinject<
+  Marker extends InjectableKey,
+  Interface,
+  Value = ValueOf<Marker> extends Interface ? Interface : ValueOf<Marker>,
+>(consumer: Consumer, mark: Marker, value: Value) {
+  for (const object of consumer.fields.values()) {
+    // SAFETY: Being in consumer.field implies this object has a provider.
+    const provider = getProvider(object)!;
+    const setters = provider.get(mark);
+    if (setters !== undefined) {
+      for (const set of setters) {
+        set(value);
+      }
+    }
+  }
+  return value;
+}
