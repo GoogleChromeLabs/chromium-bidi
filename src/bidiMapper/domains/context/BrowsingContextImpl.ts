99659679bcc0d6aed3c59798d60a7d67dd3fa4e2
Thiago Perrotta <tperrotta@chromium.org>
2023-09-27 11:38:26 -0400

    test: do not submit

--- src/bidiMapper/domains/context/BrowsingContextImpl.ts
+++ src/bidiMapper/domains/context/BrowsingContextImpl.ts
@@ -751,12 +751,6 @@ export class BrowsingContextImpl {
       ? getIntersectionRect(await this.#parseRect(params.clip), viewport)
       : viewport;
 
-    if (rect.width === 0 || rect.height === 0) {
-      throw new UnableToCaptureScreenException(
-        `Unable to capture screenshot with zero dimensions: width=${rect.width}, height=${rect.height}`
-      );
-    }
-
     const result = await this.#cdpTarget.cdpClient.sendCommand(
       'Page.captureScreenshot',
       {clip: {...rect, scale: 1.0}}
