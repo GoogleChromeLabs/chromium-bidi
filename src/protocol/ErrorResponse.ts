f79b9f36a34dc799e1333f21147da42e9a06417c
Nikolay Vitkov <nvitkov@chromium.org>
2024-01-04 20:02:35 +0100

    chore: EsLint properly lints tooling

--- src/protocol/ErrorResponse.ts
+++ src/protocol/ErrorResponse.ts
@@ -14,8 +14,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import type {ErrorResponse} from './webdriver-bidi.js';
-import {ErrorCode} from './webdriver-bidi.js';
+import type {ErrorResponse} from './cddl/webdriver-bidi.js';
+import {ErrorCode} from './cddl/webdriver-bidi.js';
 
 export class Exception {
   constructor(
