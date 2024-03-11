ca21e34f63c0bb1f96dc2c53040614e3fb0fbe31
Maksim Sadym <sadym@chromium.org>
2024-03-11 13:00:04 +0100

    feat: `invalid selector` error

--- src/bidiMapper/domains/context/BrowsingContextImpl.ts
+++ src/bidiMapper/domains/context/BrowsingContextImpl.ts
@@ -22,6 +22,7 @@ import {
   BrowsingContext,
   ChromiumBidi,
   InvalidArgumentException,
+  InvalidSelectorException,
   NoSuchElementException,
   NoSuchHistoryEntryException,
   Script,
@@ -1083,6 +1084,16 @@ export class BrowsingContextImpl {
     );
 
     if (selectorScriptResult.type !== 'success') {
+      // Heuristic to detect invalid selector.
+      if (
+        selectorScriptResult.exceptionDetails.text?.endsWith(
+          'is not a valid selector.'
+        )
+      ) {
+        throw new InvalidSelectorException(
+          `Not valid css selector ${selector}`
+        );
+      }
       throw new UnknownErrorException(
         `Unexpected error in selector script: ${selectorScriptResult.exceptionDetails.text}`
       );
