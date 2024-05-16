b5789319220de9a92d84d06620f50a4816c99fa8
Maksim Sadym <sadym@chromium.org>
2024-05-16 11:47:35 +0200

    fix locale

--- src/utils/GraphemeTools.spec.ts
+++ src/utils/GraphemeTools.spec.ts
@@ -1,5 +1,5 @@
-/**
- * Copyright 2022 Google LLC.
+/*
+ * Copyright 2024 Google LLC.
  * Copyright (c) Microsoft Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,86 +19,94 @@ import {expect} from 'chai';
 
 import {isSingleComplexGrapheme, isSingleGrapheme} from './GraphemeTools';
 
-describe('isSingleGrapheme', () => {
-  describe('should return true for a single grapheme', () => {
-    it('"😄", a single surrogate codepoint', async () => {
-      expect(isSingleGrapheme('\ud83d\ude04')).to.be.true;
+describe('GraphemeTools', () => {
+  describe('isSingleGrapheme', () => {
+    describe('should return true for a single grapheme', () => {
+      it('"😄", a single surrogate codepoint', async () => {
+        expect(isSingleGrapheme('\ud83d\ude04')).to.be.true;
+      });
+
+      it('"நி", a grapheme containing several chars', async () => {
+        expect(isSingleGrapheme('\u0BA8\u0BBF')).to.be.true;
+      });
+
+      it('"각", a grapheme containing several chars', async () => {
+        expect(isSingleGrapheme('\u1100\u1161\u11A8')).to.be.true;
+      });
+
+      it('"❤️", a grapheme containing several codepoints', async () => {
+        expect(isSingleGrapheme('\u2764\ufe0f')).to.be.true;
+      });
     });
 
-    it('"நி", a grapheme containing several chars', async () => {
-      expect(isSingleGrapheme('\u0BA8\u0BBF')).to.be.true;
-    });
+    describe('should return false for multiple graphemes', () => {
+      it('2 symbols', async () => {
+        expect(isSingleGrapheme('fa')).to.be.false;
+      });
 
-    it('"각", a grapheme containing several chars', async () => {
-      expect(isSingleGrapheme('\u1100\u1161\u11A8')).to.be.true;
-    });
+      it('"😄a" a codepoint with a symbol', async () => {
+        expect(isSingleGrapheme('\ud83d\ude04a')).to.be.false;
+      });
 
-    it('"❤️", a grapheme containing several codepoints', async () => {
-      expect(isSingleGrapheme('\u2764\ufe0f')).to.be.true;
-    });
-  });
+      it('"நிa" a grapheme with a symbol', async () => {
+        expect(isSingleGrapheme('\u0BA8\u0BBFa')).to.be.false;
+      });
 
-  describe('should return false for multiple graphemes', () => {
-    it('2 symbols', async () => {
-      expect(isSingleGrapheme('fa')).to.be.false;
-    });
+      it('"각a" a grapheme with a symbol', async () => {
+        expect(isSingleGrapheme('\u1100\u1161\u11A8a')).to.be.false;
+      });
 
-    it('"😄a" a codepoint with a symbol', async () => {
-      expect(isSingleGrapheme('\ud83d\ude04a')).to.be.false;
-    });
+      it('"❤️a" a grapheme with a symbol', async () => {
+        expect(isSingleGrapheme('\u2764\ufe0fa')).to.be.false;
+      });
 
-    it('"நிa" a grapheme with a symbol', async () => {
-      expect(isSingleGrapheme('\u0BA8\u0BBFa')).to.be.false;
-    });
+      it('"😄😍" 2 graphemes', async () => {
+        expect(isSingleGrapheme('\ud83d\ude04\ud83d\ude0d')).to.be.false;
+      });
 
-    it('"각a" a grapheme with a symbol', async () => {
-      expect(isSingleGrapheme('\u1100\u1161\u11A8a')).to.be.false;
-    });
-
-    it('"❤️a" a grapheme with a symbol', async () => {
-      expect(isSingleGrapheme('\u2764\ufe0fa')).to.be.false;
-    });
-
-    it('"😄😍" 2 graphemes', async () => {
-      expect(isSingleGrapheme('\ud83d\ude04\ud83d\ude0d')).to.be.false;
-    });
-  });
-});
-
-describe('isSingleComplexGrapheme', () => {
-  describe('should return true', function () {
-    it('"😄", a single surrogate codepoint', async () => {
-      expect(isSingleComplexGrapheme('\ud83d\ude04')).to.be.true;
-    });
-
-    it('"நி", a grapheme containing several chars', async () => {
-      expect(isSingleComplexGrapheme('\u0BA8\u0BBF')).to.be.true;
-    });
-
-    it('"각", a grapheme containing several chars', async () => {
-      expect(isSingleComplexGrapheme('\u1100\u1161\u11A8')).to.be.true;
-    });
-
-    it('"❤️", a grapheme containing several codepoints', async () => {
-      expect(isSingleComplexGrapheme('\u2764\ufe0f')).to.be.true;
+      it('"ch" 2 graphemes', async () => {
+        // https://www.unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries
+        // Spec says it's a single grapheme in slovak locale. We support only `en` locale.
+        expect(isSingleGrapheme('\ud83d\ude04\ud83d\ude0d')).to.be.false;
+      });
     });
   });
 
-  describe('should return false', function () {
-    it('"AB", 2 symbols', () => {
-      expect(isSingleComplexGrapheme('AB')).to.be.false;
+  describe('isSingleComplexGrapheme', () => {
+    describe('should return true', () => {
+      it('"😄", a single surrogate codepoint', async () => {
+        expect(isSingleComplexGrapheme('\ud83d\ude04')).to.be.true;
+      });
+
+      it('"நி", a grapheme containing several chars', async () => {
+        expect(isSingleComplexGrapheme('\u0BA8\u0BBF')).to.be.true;
+      });
+
+      it('"각", a grapheme containing several chars', async () => {
+        expect(isSingleComplexGrapheme('\u1100\u1161\u11A8')).to.be.true;
+      });
+
+      it('"❤️", a grapheme containing several codepoints', async () => {
+        expect(isSingleComplexGrapheme('\u2764\ufe0f')).to.be.true;
+      });
     });
 
-    it('"A", a single simple symbol', () => {
-      expect(isSingleComplexGrapheme('A')).to.be.false;
-    });
+    describe('should return false', () => {
+      it('"AB", 2 symbols', () => {
+        expect(isSingleComplexGrapheme('AB')).to.be.false;
+      });
 
-    it('"1", a single simple symbol', () => {
-      expect(isSingleComplexGrapheme('1')).to.be.false;
-    });
+      it('"A", a single simple symbol', () => {
+        expect(isSingleComplexGrapheme('A')).to.be.false;
+      });
 
-    it('empty string', () => {
-      expect(isSingleComplexGrapheme('')).to.be.false;
+      it('"1", a single simple symbol', () => {
+        expect(isSingleComplexGrapheme('1')).to.be.false;
+      });
+
+      it('empty string', () => {
+        expect(isSingleComplexGrapheme('')).to.be.false;
+      });
     });
   });
 });
--- src/utils/GraphemeTools.ts
+++ src/utils/GraphemeTools.ts
@@ -15,11 +15,21 @@
  * limitations under the License.
  */
 
+/**
+ * Check if the given string is a single complex grapheme. A complex grapheme is one that
+ * is made up of multiple characters.
+ */
 export function isSingleComplexGrapheme(value: string): boolean {
   return isSingleGrapheme(value) && value.length > 1;
 }
 
+/**
+ * Check if the given string is a single grapheme.
+ */
 export function isSingleGrapheme(value: string): boolean {
-  const segmenter = new Intl.Segmenter(undefined, {granularity: 'grapheme'});
+  // Theoretically there can be some strings considered a grapheme in some locales, like
+  // slovak "ch" digraph. Use english locale for consistency.
+  // https://www.unicode.org/reports/tr29/#Grapheme_Cluster_Boundaries
+  const segmenter = new Intl.Segmenter('en', {granularity: 'grapheme'});
   return [...segmenter.segment(value)].length === 1;
 }
