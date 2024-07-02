a3f2f68b73737e598f4b99ca7ac458c8ff1a40c0
Maksim Sadym <sadym@chromium.org>
2024-07-02 14:09:10 +0200

    remove `html_data` fixture

--- tests/browsing_context/test_traverse_history.py
+++ tests/browsing_context/test_traverse_history.py
@@ -21,28 +21,32 @@ HISTORY_LENGTH = 2
 
 
 @pytest.mark.asyncio
-async def test_traverse_history(websocket, context_id, html_data):
+async def test_traverse_history(websocket, context_id):
+    urls = []
     for i in range(HISTORY_LENGTH + 1):
-        await goto_url(websocket, context_id, html_data(i))
+        # TODO: use `html` fixture instead.
+        url = f'data:text/html,{i}'
+        urls.append(url)
+        await goto_url(websocket, context_id, url)
 
     await subscribe(websocket, ["browsingContext.load"])
 
     await traverse_history(websocket, context_id, -2)
-    await assert_href_equals(websocket, html_data(HISTORY_LENGTH - 2))
+    await assert_href_equals(websocket, urls[HISTORY_LENGTH - 2])
 
     await traverse_history(websocket, context_id, 2)
-    await assert_href_equals(websocket, html_data(HISTORY_LENGTH))
+    await assert_href_equals(websocket, urls[HISTORY_LENGTH])
 
     await traverse_history(websocket, context_id, -1)
-    await assert_href_equals(websocket, html_data(HISTORY_LENGTH - 1))
+    await assert_href_equals(websocket, urls[HISTORY_LENGTH - 1])
 
     await traverse_history(websocket, context_id, 1)
-    await assert_href_equals(websocket, html_data(HISTORY_LENGTH))
+    await assert_href_equals(websocket, urls[HISTORY_LENGTH])
 
     # There is no event here.
     await traverse_history(websocket, context_id, 0)
     await assert_location_href_equals(websocket, context_id,
-                                      html_data(HISTORY_LENGTH))
+                                      urls[HISTORY_LENGTH])
 
 
 @pytest.mark.asyncio
