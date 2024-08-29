c33c7fd115d21ba848241a436c426b9fa3d6f2bb
Maksim Sadym <sadym@chromium.org>
2024-08-29 13:20:08 +0200

    fix: css and xPath selector start node is document

--- src/bidiMapper/modules/context/BrowsingContextImpl.ts
+++ src/bidiMapper/modules/context/BrowsingContextImpl.ts
@@ -1255,7 +1255,7 @@ export class BrowsingContextImpl {
                 return [...element.querySelectorAll(cssSelector)];
               };
 
-              startNodes = startNodes.length > 0 ? startNodes : [document.body];
+              startNodes = startNodes.length > 0 ? startNodes : [document];
               const returnedNodes = startNodes
                 .map((startNode) =>
                   // TODO: stop search early if `maxNodeCount` is reached.
@@ -1298,7 +1298,7 @@ export class BrowsingContextImpl {
                 }
                 return returnedNodes;
               };
-              startNodes = startNodes.length > 0 ? startNodes : [document.body];
+              startNodes = startNodes.length > 0 ? startNodes : [document];
               const returnedNodes = startNodes
                 .map((startNode) =>
                   // TODO: stop search early if `maxNodeCount` is reached.
