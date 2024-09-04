328171b3eff9dad238ea4e88f9ae4ff58ff2ace8
Alex N. Jose <alexnj@google.com>
2024-09-04 06:06:58 -0700

    Update with assertion

--- tests/bluetooth/test_handle_prompt.py
+++ tests/bluetooth/test_handle_prompt.py
@@ -14,57 +14,60 @@
 #  limitations under the License.
 
 import pytest
-from test_helpers import (execute_command, goto_url, send_JSON_command,
-                          subscribe, wait_for_event)
+from test_helpers import (AnyExtending, execute_command, goto_url,
+                          send_JSON_command, subscribe, wait_for_event)
+
+HTML_SINGLE_PERIPHERAL = """
+<div>
+    <a href="#" id="bluetooth" target="_blank">bluetooth</a>
+    <script>
+        var options = {filters: [{name:"SomeDevice"}]};
+        document.getElementById('bluetooth').addEventListener('click', () => {
+        navigator.bluetooth.requestDevice(options);
+    });
+    </script>
+</div>
+"""
 
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize('capabilities', [{
-    "goog:chromeOptions": {
-        "args": ["--enable-features=WebBluetooth"]
+    'goog:chromeOptions': {
+        'args': ['--enable-features=WebBluetooth']
     }
 }],
                          indirect=True)
 async def test_bluetooth_handle_prompt(websocket, context_id, html):
-    await subscribe(websocket, ["bluetooth"])
+    await subscribe(websocket, ['bluetooth'])
 
-    url = html(
-        "<div>"
-        '<a href="#" id="bluetooth" target="_blank">bluetooth</a>'
-        "<script>"
-        'var options = {filters: [{name:"SomeDevice"}]};'
-        "document.getElementById('bluetooth').addEventListener('click', () => {"
-        "navigator.bluetooth.requestDevice(options);"
-        "});"
-        "</script>"
-        "</div>")
+    url = html(HTML_SINGLE_PERIPHERAL)
     await goto_url(websocket, context_id, url)
 
     # Enable BT emulation.
     await execute_command(
         websocket, {
-            "method": "cdp.sendCommand",
-            "params": {
-                "method": "BluetoothEmulation.enable",
-                "params": {
-                    "state": 'powered-on',
+            'method': 'cdp.sendCommand',
+            'params': {
+                'method': 'BluetoothEmulation.enable',
+                'params': {
+                    'state': 'powered-on',
                 }
             }
         })
 
     # Create a fake BT device.
-    fake_device_address = "09:09:09:09:09:09"
+    fake_device_address = '09:09:09:09:09:09'
     await execute_command(
         websocket, {
-            "method": "cdp.sendCommand",
-            "params": {
-                "method": "BluetoothEmulation.simulatePreconnectedPeripheral",
-                "params": {
-                    "address": fake_device_address,
-                    "name": "SomeDevice",
-                    "manufacturerData": [],
-                    "knownServiceUuids":
-                        ["12345678-1234-5678-9abc-def123456789", ],
+            'method': 'cdp.sendCommand',
+            'params': {
+                'method': 'BluetoothEmulation.simulatePreconnectedPeripheral',
+                'params': {
+                    'address': fake_device_address,
+                    'name': 'SomeDevice',
+                    'manufacturerData': [],
+                    'knownServiceUuids':
+                        ['12345678-1234-5678-9abc-def123456789', ],
                 }
             }
         })
@@ -82,4 +85,15 @@ async def test_bluetooth_handle_prompt(websocket, context_id, html):
             }
         })
 
-    await wait_for_event(websocket, "bluetooth.requestDevicePromptOpened")
+    response = await wait_for_event(websocket,
+                                    'bluetooth.requestDevicePromptOpened')
+    assert response == AnyExtending({
+        'type': 'event',
+        'method': 'bluetooth.requestDevicePromptOpened',
+        'params': {
+            'context': context_id,
+            'devices': [{
+                'id': '09:09:09:09:09:09'
+            }],
+        }
+    })
