8133f84dfc4dbb261ca694f56bb018693fb98130
Maksim Sadym <sadym@chromium.org>
2024-12-13 11:10:54 +0100

    e2e beforeunload + historyUpdated + reload

--- tests/browsing_context/test_history_updated.py
+++ tests/browsing_context/test_history_updated.py
@@ -13,8 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import pytest
-from test_helpers import (goto_url, read_JSON_message, send_JSON_command,
-                          subscribe)
+from test_helpers import (ANY_TIMESTAMP, execute_command, goto_url,
+                          read_JSON_message, send_JSON_command,
+                          stabilize_key_values, subscribe)
 
 
 @pytest.mark.asyncio
@@ -45,3 +46,80 @@ async def test_browsingContext_historyUpdated_event(websocket, context_id,
             "url": url_base + "#test",
         }
     }
+
+
+@pytest.mark.asyncio
+async def test_browsingContext_beforeunload_historyUpdated_reload_event(
+        websocket, context_id, url_base, read_messages):
+    await subscribe(websocket, ["browsingContext"])
+
+    await goto_url(websocket, context_id, url_base)
+
+    await execute_command(
+        websocket, {
+            "method": "script.evaluate",
+            "params": {
+                "expression": """
+                    window.addEventListener('beforeunload', () => {
+                        return history.replaceState(null, 'initial', '#test');
+                    },false)
+                """,
+                "target": {
+                    "context": context_id,
+                },
+                "awaitPromise": False
+            }
+        })
+
+    command_id = await send_JSON_command(websocket, {
+        "method": "browsingContext.reload",
+        "params": {
+            "context": context_id
+        }
+    })
+
+    messages = await read_messages(5, check_no_other_messages=True, sort=False)
+    stabilize_key_values(messages, ['navigation'])
+
+    assert messages == [{
+        'id': command_id,
+        'type': "success",
+        'result': {
+            'navigation': 'stable_0',
+            'url': url_base,
+        },
+    }, {
+        'method': 'browsingContext.historyUpdated',
+        'params': {
+            'context': context_id,
+            'url': url_base + "#test",
+        },
+        'type': 'event',
+    }, {
+        'method': 'browsingContext.navigationStarted',
+        'params': {
+            'context': context_id,
+            'navigation': 'stable_0',
+            'timestamp': ANY_TIMESTAMP,
+            'url': url_base,
+        },
+        'type': 'event',
+    }, {
+        'method': 'browsingContext.domContentLoaded',
+        'params': {
+            'context': context_id,
+            'navigation': 'stable_0',
+            'timestamp': ANY_TIMESTAMP,
+            'url': url_base,
+        },
+        'type': 'event',
+    }, {
+        'method': 'browsingContext.load',
+        'params': {
+            'context': context_id,
+            'navigation': 'stable_0',
+            'timestamp': ANY_TIMESTAMP,
+            'url': url_base,
+        },
+        'type': 'event',
+    }]
