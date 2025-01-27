3d7c7339581c102bc959e5ad98d41e415d819c2d
Liviu Rau <liviurau@google.com>
2025-01-27 16:19:35 +0100

    Add e2e tests

--- src/bidiMapper/modules/webExtension/WebExtensionProcessor.ts
+++ src/bidiMapper/modules/webExtension/WebExtensionProcessor.ts
@@ -15,30 +15,15 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2023 Google LLC.
- * Copyright (c) Microsoft Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 import type {CdpClient} from '../../../cdp/CdpClient.js';
 import {
   InvalidWebExtensionException,
   type WebExtension,
+  UnsupportedOperationException,
 } from '../../../protocol/protocol.js';
 
 /**
- * Responsible for handling the `storage` module.
+ * Responsible for handling the `webModule` module.
  */
 export class WebExtensionProcessor {
   readonly #browserCdpClient: CdpClient;
@@ -50,19 +35,26 @@ export class WebExtensionProcessor {
   async install(
     params: WebExtension.InstallParameters,
   ): Promise<WebExtension.InstallResult> {
+    if (!(params.extensionData as WebExtension.ExtensionPath).path) {
+      throw new UnsupportedOperationException(
+        'Archived and Base64 extensions are not supported',
+      );
+    }
     try {
       const response = await this.#browserCdpClient.sendCommand(
         'Extensions.loadUnpacked',
         {
-          path: (params.extensionData as WebExtension.ExtensionArchivePath)
-            .path,
+          path: (params.extensionData as WebExtension.ExtensionPath).path,
         },
       );
       return {
         extension: response.id,
       };
     } catch (err: any) {
-      throw new InvalidWebExtensionException(err.toString());
+      if ((err as Error).message.startsWith('invalid web extension')) {
+        throw new InvalidWebExtensionException((err as Error).message);
+      }
+      throw err;
     }
   }
 }
