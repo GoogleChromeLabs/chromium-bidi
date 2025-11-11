2da7f7d509cd2be41f287afa530abf74604b7eca
github-action-benchmark <github@users.noreply.github.com>
2025-11-11 13:58:25 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 1495ddb0a2c89bb3e1608e6f550414fca3ff4159

---
+++ dev/bench/data.js
@@ -0,0 +1,83 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1762869505908,
+  "repoUrl": "https://github.com/GoogleChromeLabs/chromium-bidi",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "GoogleChromeLabs",
+            "username": "GoogleChromeLabs"
+          },
+          "committer": {
+            "name": "GoogleChromeLabs",
+            "username": "GoogleChromeLabs"
+          },
+          "id": "1495ddb0a2c89bb3e1608e6f550414fca3ff4159",
+          "message": "chore: publish performance data to gh-pages",
+          "timestamp": "2025-11-11T12:09:44Z",
+          "url": "https://github.com/GoogleChromeLabs/chromium-bidi/pull/3907/commits/1495ddb0a2c89bb3e1608e6f550414fca3ff4159"
+        },
+        "date": 1762869505460,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "macos-latest-headful-cd:test_performance_screenshot",
+            "value": 3065.855646133423,
+            "unit": "ms"
+          },
+          {
+            "name": "macos-latest-headful-node:test_performance_screenshot",
+            "value": 2518.8737869262695,
+            "unit": "ms"
+          },
+          {
+            "name": "macos-latest-new-headless-cd:test_performance_screenshot",
+            "value": 2438.2740020751953,
+            "unit": "ms"
+          },
+          {
+            "name": "macos-latest-new-headless-node:test_performance_screenshot",
+            "value": 1946.3792324066162,
+            "unit": "ms"
+          },
+          {
+            "name": "macos-latest-old-headless-cd:test_performance_screenshot",
+            "value": 335.1961612701416,
+            "unit": "ms"
+          },
+          {
+            "name": "macos-latest-old-headless-node:test_performance_screenshot",
+            "value": 343.37878227233887,
+            "unit": "ms"
+          },
+          {
+            "name": "ubuntu-latest-headful-node:test_performance_screenshot",
+            "value": 151.82175636291504,
+            "unit": "ms"
+          },
+          {
+            "name": "ubuntu-latest-new-headless-cd:test_performance_screenshot",
+            "value": 184.49444770812988,
+            "unit": "ms"
+          },
+          {
+            "name": "ubuntu-latest-new-headless-node:test_performance_screenshot",
+            "value": 174.7650146484375,
+            "unit": "ms"
+          },
+          {
+            "name": "ubuntu-latest-old-headless-cd:test_performance_screenshot",
+            "value": 281.3192844390869,
+            "unit": "ms"
+          },
+          {
+            "name": "ubuntu-latest-old-headless-node:test_performance_screenshot",
+            "value": 274.8520851135254,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
