9a223bcfcbece15333feac63c6d8a06baad5975a
Maksim Sadym <sadym@chromium.org>
2026-01-23 17:22:21 +0100

    qwe

--- tools/benchmark-puppeteer.mjs
+++ tools/benchmark-puppeteer.mjs
@@ -168,7 +168,7 @@ async function runBenchmarkRun(name, launchOptions, chromePath) {
 
 async function main() {
   // Headless shell does not work with Puppeteer. Use new headless.
-  const chromePath = installAndGetChromePath(false);
+  const chromePath = installAndGetChromePath();
   console.log(`Using Chrome: ${chromePath}`);
   console.log(
     `Starting Benchmark: ${RUNS} runs x ${ITERATIONS_PER_RUN} iterations...`,
